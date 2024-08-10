3005e95cae4f1cca2c46fb3dda3befecef283230
JaeYun Moon <108652767+tenedict@users.noreply.github.com>
2024-08-11 00:00:06 +0900

    Update views.py

--- chatgpt/views.py
+++ chatgpt/views.py
@@ -5,8 +5,14 @@ from django.http import JsonResponse
 import openai
 import os
 
-openai.api_key = 'sk-svcacct-wqFvghT-PlMbVjYhsHtgjSER1UPsILezxPbblZkFS343ST3BlbkFJCucjx-EWBkDwor3rdjzLsVP6-BPgaJBOqHnfpcstHL4AA'
 
+from dotenv import load_dotenv
+
+# .env 파일에서 환경 변수 로드하기
+load_dotenv()
+
+# 환경 변수에서 API 키 가져오기
+openai.api_key = os.getenv('OPENAI_API_KEY')
 def get_completion(prompt): 
 	print(prompt) 
 	query = openai.ChatCompletion.create( 
