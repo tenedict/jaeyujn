0fe7cb05fe847fd437482a3dad68f9f1f2820e9d
JaeYun Moon <108652767+tenedict@users.noreply.github.com>
2024-08-11 05:21:32 +0900

    Update settings.py

--- canieat/settings.py
+++ canieat/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'default-secret-key')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
